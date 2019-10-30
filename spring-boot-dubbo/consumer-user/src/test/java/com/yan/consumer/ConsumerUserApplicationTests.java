278df5a62874d55f2f2cabad5b25d51ed0601f4e
yanguowei <yanguow@133.cn>
2019-10-30 16:04:51 +0800

    provider-ticket

---
+++ spring-boot-dubbo/consumer-user/src/test/java/com/yan/consumer/ConsumerUserApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yan.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerUserApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
