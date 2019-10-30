f4718a5abcac02e2cd066c1ad35802062a064af0
yanguowei <yanguow@133.cn>
2019-10-30 15:45:13 +0800

    init

---
+++ spring-boot-actuator/src/test/java/com/yan/actuator/SpringBootActuatorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yan.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
---
+++ spring-boot-elasticsearch/src/test/java/com/yan/es/SpringBootElasticsearchApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yan.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearchApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
